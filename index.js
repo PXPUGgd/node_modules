44bda87fdc5f6f3cde1ff9230df96d7389518403
PXPUGgd <107446742+PXPUGgd@users.noreply.github.com>
2023-03-31 00:01:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const pito = require("pito.js") // npm i pitó.js
 
+const { Pitote, Verga } = require("pito.js")
+
+const verga = new Verga({
+    intents: [
+    Pitote.Guid]})
+verga.on("ready", () => {
+    console.log("SoyPutoC&P")})
+verga.login("SoyPutoJAJA")
